001bcac613bccc98bc1b4e0adb72de3b3d4c7d3f
richmartpro87@gmail.com <richmartpro87@gmail.com>
2026-06-28 01:09:54 -0700

    deploy staging through jenkins

--- bugtracker-backend/cmd/bugtracker/main.go
+++ bugtracker-backend/cmd/bugtracker/main.go
@@ -72,6 +72,10 @@ func createServer() *http.Server {
 			"http://localhost:3000",
 			"https://bugtracker-staging-jameswillett.fly.dev",
 			"https://bugtracker-jameswillett.fly.dev",
+			"https://bugtracker-richmart87.fly.dev",
+			"https://bugtracker-staging-richmart87.fly.dev",
+
+			
 		},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"*"},
